923f58aaa0a4bfc9642ac6c22756e9d46d057e9a
Ollie Batchelor <olliebatchelor@hotmail.com>
2022-05-18 10:08:02 +0100

    rename prop

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,15 +2,14 @@ import React, {Component} from "react";
 
 class QuizQuestion extends Component {
     render() {
-        console.log(this.props)
         return (
             <main>
                 <section>
-                    <p>{this.props.quizQuestion.instruction_text}</p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>{this.props.quizQuestion.answer_options[0]}</li>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
                     </ul>
                 </section>
             </main>)
