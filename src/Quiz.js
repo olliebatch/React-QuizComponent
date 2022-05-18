84bdfed7226c5f5f8e57e1cfaf7742e7cbab67f2
Ollie Batchelor <olliebatchelor@hotmail.com>
2022-05-18 10:26:34 +0100

    match course

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,11 +12,11 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
+        const isQuizEnd = (quizData.quiz_questions.length === (this.state.quiz_position - 1));
 
         return <div>
-            {isQuizEnd ? <QuizEnd/> : null}
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            {isQuizEnd ? <QuizEnd/> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
         </div>
     }
 }
